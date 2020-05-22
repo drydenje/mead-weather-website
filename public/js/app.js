11c2634d32550d0344fc1056bf72a48a2caa22ae
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-22 06:56:04 -0400

    Finished 4.67

--- public/js/app.js
+++ public/js/app.js
@@ -4,18 +4,16 @@ const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
 const getWeather = (location = "") => {
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          return (messageOne.textContent = data.error);
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        return (messageOne.textContent = data.error);
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 };
 
 weatherForm.addEventListener("submit", (e) => {
